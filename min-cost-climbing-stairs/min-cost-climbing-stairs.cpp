da5ba5aafa6323e1a2456c1bbd9ed55d31046550
Varun Pandey <98257202+varunpandey29@users.noreply.github.com>
2023-07-14 00:52:53 +0530

    Time: 7 ms (65.08%) | Memory: 13.8 MB (17.17%) - LeetSync

--- min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int minCost(vector<int>& cost, int n, vector<int>& dp)
+    int minCost(vector<int>& cost, int n)
     {
-        if(n<=1)
-            return cost[n];
-            
-        //Step 3 
-        if(dp[n] != -1)
-            return dp[n];
-
-        //Step 2 - memoization - storing values in dp array
-        dp[n] = cost[n] + min(minCost(cost, n-1, dp), minCost(cost, n-2, dp));
-        return dp[n];
+        // Step-1 : creation od DP array;
+        vector<int> dp(n+1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i<n; i++)
+        {
+            dp[i] = cost[i] + min(dp[i-1] , dp[i-2]);
+        }
+        return min(dp[n-1], dp[n-2]);
     }
 
      
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();   
 
-        //STEP 1 - initialize DP of size (n+1) and initialized with -1
-        vector<int> dp(n+1, -1);       
-        int ans =  min(minCost(cost, n-1, dp), minCost(cost, n-2, dp));
-        return ans;
+        //STEP 1 - define a DP array of size (n+1) and initialized with -1
+        // vector<int> dp(n+1, -1);       
+        // int ans =  min(minCost(cost, n-1, dp), minCost(cost, n-2, dp));
+        return minCost(cost, n);
     }
 };
